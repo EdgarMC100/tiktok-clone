b05ada9c9a0668d9d9b02f8455818add0b076f60
edgarmc <edgarmc100@gmail.com>
2022-03-22 20:33:44 -0600

    feat(Root): Adds Icons and styles

--- src/components/FeedVideo/index.jsx
+++ src/components/FeedVideo/index.jsx
@@ -11,7 +11,7 @@ const VIDEOS = [
     songTitle: 'sonido original',
     albumCover: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/eacc7e37ffee1c3052e0ff793b8e88a5~c5_100x100.jpeg?x-expires=1647374400&x-signature=EUmaT2oTE6gOMjSXd4BWFNVHZpc%3D',
     description: 'Using React to develop websites versus HTML and CSS 👨🏼‍💻',
-    src: 'https://v16-webapp.tiktok.com/8941317584f064f2d6364b91873b0265/6233bc73/video/tos/useast2a/tos-useast2a-ve-0068c003/02231ed20d2b4ae3bc3a23591164add3/?a=1988&br=1558&bt=779&cd=0|0|1|0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3CMnz7ThJneiDXq&l=20220317165517010189077068013443E8&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajpvcTk6ZmhqOTMzNzczM0ApZTM2Njk5ZTtkNzNlOzMzOGdgNWBpcjQwbF9gLS1kMTZzczUuLjNhNjJjYy1hYmEzMzA6Yw==&vl=&vr='
+    src: 'https://v16-webapp.tiktok.com/cbc5497d3612e2b9a67727d5f267bb76/623ad775/video/tos/useast2a/tos-useast2a-ve-0068c003/cf315099d217445c8f69076c2e7f26e4/?a=1988&br=1876&bt=938&cd=0|0|1|0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3CMnz7ThPALMDXq&l=2022032302163701022311902020967F59&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M29yb2U6Znd1OzMzNzczM0ApNjYzZGk2NTxnNzM4ZDVkZWdyXnJscjRnMG1gLS1kMTZzc2FfMV80YzI0NGA1NC9gNTI6Yw==&vl=&vr='
 
   },
   {
@@ -23,7 +23,7 @@ const VIDEOS = [
     songTitle: 'sonido original',
     albumCover: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/eacc7e37ffee1c3052e0ff793b8e88a5~c5_100x100.jpeg?x-expires=1647374400&x-signature=EUmaT2oTE6gOMjSXd4BWFNVHZpc%3D',
     description: 'Using React to develop websites versus HTML and CSS 👨🏼‍💻',
-    src: 'https://v16-webapp.tiktok.com/8941317584f064f2d6364b91873b0265/6233bc73/video/tos/useast2a/tos-useast2a-ve-0068c003/02231ed20d2b4ae3bc3a23591164add3/?a=1988&br=1558&bt=779&cd=0|0|1|0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3CMnz7ThJneiDXq&l=20220317165517010189077068013443E8&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajpvcTk6ZmhqOTMzNzczM0ApZTM2Njk5ZTtkNzNlOzMzOGdgNWBpcjQwbF9gLS1kMTZzczUuLjNhNjJjYy1hYmEzMzA6Yw==&vl=&vr='
+    src: 'https://v16-webapp.tiktok.com/cbc5497d3612e2b9a67727d5f267bb76/623ad775/video/tos/useast2a/tos-useast2a-ve-0068c003/cf315099d217445c8f69076c2e7f26e4/?a=1988&br=1876&bt=938&cd=0|0|1|0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3CMnz7ThPALMDXq&l=2022032302163701022311902020967F59&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M29yb2U6Znd1OzMzNzczM0ApNjYzZGk2NTxnNzM4ZDVkZWdyXnJscjRnMG1gLS1kMTZzc2FfMV80YzI0NGA1NC9gNTI6Yw==&vl=&vr='
   }
 
 ]
--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { Heart } from '../Icons/Heart'
+import Heart from '../Icons/Heart'
+import Share from '../Icons/Share'
+import Comments from '../Icons/Comments'
 import styles from './styles.module.css'
-export default function VideoPlayerActions () {
+export default function VideoPlayerActions ({ likes = 2304, comments = 2020, shares = 12, hearted = false }) {
   return (
     <aside className={styles.actions}>
       <div className={styles.action}>
         <Heart />
+        <strong title='like'>{likes}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Comments />
+        <strong title='shares'>{shares}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Share />
+        <strong title='comments'>{comments}</strong>
       </div>
     </aside>
   )
