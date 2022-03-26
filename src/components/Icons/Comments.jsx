453deb76f2566165dafb310dc4669ff881a9f21e
edgarmc <edgarmc100@gmail.com>
2022-03-25 18:27:42 -0600

    feat(Root): Adds styles for music icons

--- src/components/Icons/Comments.jsx
+++ src/components/Icons/Comments.jsx
@@ -5,7 +5,6 @@ export default function Comments (props) {
     <svg
       xmlns='http://www.w3.org/2000/svg'
       fill='none'
-      width='40' height='40'
       viewBox='0 0 48 48'
       {...props}
     >
